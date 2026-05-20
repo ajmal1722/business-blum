3b3b2518bed1d613a3f348302e1b5a3cb5e46150
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2026-05-20 18:21:24 +0530

    feat: CTA section created

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import HowItWorks from "@/components/Homepage/HowItWorks";
 import BusinessCategories from "@/components/Homepage/BusinessCategories";
 import WhyChoose from "@/components/Homepage/WhyChooseUs";
 import Testimonials from "@/components/Homepage/Testimonials";
+import CTASection from "@/components/Homepage/CTASection";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <BusinessCategories />
       <WhyChoose />
       <Testimonials />
+      <CTASection />
     </>
   );
 }
